68216c61f4f6f15d72aa64a60a804d800301fa7c
hemanta12 <hemantathapa@Hemantas-MacBook-Pro.local>
2024-08-12 18:26:56 -0700

    Initial commit

---
+++ backend/src/main/java/com/dumpnotes/backend/DumpNotesApplication.java
@@ -0,0 +1,13 @@
+package com.dumpnotes.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DumpNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DumpNotesApplication.class, args);
+	}
+
+}
