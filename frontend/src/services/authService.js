85727869e4be78775fc2c71c26104a6b0d7b0976
hemanta12 <hemantathapa@Hemantas-MacBook-Pro.local>
2024-12-13 13:11:41 -0800

    added env files and updates auth.js and dashboard.js to work with backend on render

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
-// const API_URL = 'http://localhost:8080/api/auth';
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080/api';
-
+const API_URL = process.env.REACT_APP_API_URL;
 
 const login = async (credentials) => {
     const response = await axios.post(`${API_URL}/login`, credentials, {
         withCredentials: true,
+        headers: {
+            'Content-Type': 'application/json',
+        }
     });
-    // Assuming the response contains the username
     if (response.data && response.data.username) {
         localStorage.setItem("username", response.data.username);
         localStorage.setItem('isAuthenticated', 'true');
@@ -16,28 +16,33 @@ const login = async (credentials) => {
     return response.data;
 };
 
-const register = async (userData) =>{
+const register = async (userData) => {
     const response = await axios.post(`${API_URL}/register`, userData, {
         withCredentials: true,
+        headers: {
+            'Content-Type': 'application/json',
+        }
     });
     return response.data;
 };
 
-const logout = async() =>{
-    await axios.post(`${API_URL}/logout`, {} , {
+const logout = async () => {
+    await axios.post(`${API_URL}/logout`, {}, {
         withCredentials: true,
+        headers: {
+            'Content-Type': 'application/json',
+        }
     });
-    // Clear the username from local storage on logout
     localStorage.removeItem("username");
     localStorage.removeItem('isAuthenticated');
 };
+
 const isAuthenticated = () => {
     return localStorage.getItem('isAuthenticated') === 'true';
-  };
-
-const authService =  {
-  login, register, logout,isAuthenticated
 };
 
+const authService = {
+    login, register, logout, isAuthenticated
+};
 
 export default authService;
--- frontend/src/services/dashboardService.js
+++ frontend/src/services/dashboardService.js
@@ -2,14 +2,25 @@
 
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/notes';
+const API_URL = `${process.env.REACT_APP_API_URL}/notes`;
 
 const getNotes = async () => {
-    const response = await axios.get(API_URL, { withCredentials: true });
+    const response = await axios.get(API_URL, {
+        withCredentials: true,
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    });
     return response.data;
 };
+
 const createNote = async (noteData) => {
-    const response = await axios.post(API_URL, noteData, { withCredentials: true });
+    const response = await axios.post(API_URL, noteData, {
+        withCredentials: true,
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    });
     return response.data;
 };
 
